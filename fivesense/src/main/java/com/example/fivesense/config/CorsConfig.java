a3cbb7a52b32b14b9d7966a3a18b9b873d93ee25
Ajms1104 <164110764+Ajms1104@users.noreply.github.com>
2025-09-29 17:03:29 +0900

    backend, frontend ci 수정

--- fivesense/src/main/java/com/example/fivesense/config/CorsConfig.java
+++ fivesense/src/main/java/com/example/fivesense/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로(/**)에 대해 CORS 설정을 적용
                 .allowedOrigins("http://localhost:5173") // React 개발 서버 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메서드 종류를 명시합니다.
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메서드 종류
                 .allowedHeaders("*") // 모든 종류의 HTTP 헤더를 허용합니다.
                 .allowCredentials(true) // 자격 증명(쿠키 등) 정보의 전송을 허용합니다.
                 .maxAge(3600); // Pre-flight 요청의 결과를 캐싱할 시간(초)을 설정합니다.
